804e8481c088e0cbf000f14733373c109ae270a3
Andreeha <ofpprts@mail.ru>
2025-10-18 12:35:13 +0300

    Moved tests out of src folder

--- build.sh
+++ build.sh
@@ -10,6 +10,7 @@ INCLUDE="-I./external/zlib -I ./include"
 DEBUG="-ggdb -Wno-incompatible-pointer-types -Wno-int-conversion -Wno-discarded-qualifiers"
 BUILD="./build/"
 SRC="./src/"
+TESTS="./tests/"
 
 echo [COMPILE] ${SRC}rb.c
 gcc -c ${SRC}rb.c -o ./lib/rb.o $INCLUDE $DEBUG
@@ -21,46 +22,46 @@ gcc -c ${SRC}compressor.c -o ./lib/compressor.o $INCLUDE $DEBUG
 
 TARGET=create_table
   echo [BUILD] ${src}${TARGET}.c
-  gcc -o ${BUILD}${TARGET} ${SRC}${TARGET}.c $INCLUDE $LIBS $DEBUG
+  gcc -o ${BUILD}${TARGET} ${TESTS}${TARGET}.c $INCLUDE $LIBS $DEBUG
 
 TARGET=erase_table
   echo [BUILD] ${src}${TARGET}.c
-  gcc -o ${BUILD}${TARGET} ${SRC}${TARGET}.c $INCLUDE $LIBS $DEBUG
+  gcc -o ${BUILD}${TARGET} ${TESTS}${TARGET}.c $INCLUDE $LIBS $DEBUG
 
 TARGET=delete_table
   echo [BUILD] ${src}${TARGET}.c
-  gcc -o ${BUILD}${TARGET} ${SRC}${TARGET}.c $INCLUDE $LIBS $DEBUG
+  gcc -o ${BUILD}${TARGET} ${TESTS}${TARGET}.c $INCLUDE $LIBS $DEBUG
 
 # Entry functions
 
 TARGET=insert
   echo [BUILD] ${src}${TARGET}.c
-  gcc -o ${BUILD}${TARGET} ${SRC}${TARGET}.c $INCLUDE $LIBS $DEBUG
+  gcc -o ${BUILD}${TARGET} ${TESTS}${TARGET}.c $INCLUDE $LIBS $DEBUG
 
 TARGET=delete
   echo [BUILD] ${src}${TARGET}.c
-  gcc -o ${BUILD}${TARGET} ${SRC}${TARGET}.c $INCLUDE $LIBS $DEBUG
+  gcc -o ${BUILD}${TARGET} ${TESTS}${TARGET}.c $INCLUDE $LIBS $DEBUG
 
 TARGET=delete_many
   echo [BUILD] ${src}${TARGET}.c
-  gcc -o ${BUILD}${TARGET} ${SRC}${TARGET}.c $INCLUDE $LIBS $DEBUG
+  gcc -o ${BUILD}${TARGET} ${TESTS}${TARGET}.c $INCLUDE $LIBS $DEBUG
 
 TARGET=find
   echo [BUILD] ${src}${TARGET}.c
-  gcc -o ${BUILD}${TARGET} ${SRC}${TARGET}.c $INCLUDE $LIBS $DEBUG
+  gcc -o ${BUILD}${TARGET} ${TESTS}${TARGET}.c $INCLUDE $LIBS $DEBUG
 
 TARGET=find_many
   echo [BUILD] ${src}${TARGET}.c
-  gcc -o ${BUILD}${TARGET} ${SRC}${TARGET}.c $INCLUDE $LIBS $DEBUG
+  gcc -o ${BUILD}${TARGET} ${TESTS}${TARGET}.c $INCLUDE $LIBS $DEBUG
 
 TARGET=edit
   echo [BUILD] ${src}${TARGET}.c
-  gcc -o ${BUILD}${TARGET} ${SRC}${TARGET}.c $INCLUDE $LIBS $DEBUG
+  gcc -o ${BUILD}${TARGET} ${TESTS}${TARGET}.c $INCLUDE $LIBS $DEBUG
 
 TARGET=edit_many
   echo [BUILD] ${src}${TARGET}.c
-  gcc -o ${BUILD}${TARGET} ${SRC}${TARGET}.c $INCLUDE $LIBS $DEBUG
+  gcc -o ${BUILD}${TARGET} ${TESTS}${TARGET}.c $INCLUDE $LIBS $DEBUG
 
 TARGET=backup
   echo [BUILD] ${src}${TARGET}.c
-  gcc -o ${BUILD}${TARGET} ${SRC}${TARGET}.c $INCLUDE $LIBS $DEBUG
+  gcc -o ${BUILD}${TARGET} ${TESTS}${TARGET}.c $INCLUDE $LIBS $DEBUG
